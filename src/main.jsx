aad0a2a95599cc68bb0765bcbee416265f1635a5
Neeraj Patil <110386298+Neeraaaj@users.noreply.github.com>
2023-11-23 17:27:39 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import Login from './login.jsx'
+import Login from './Login.jsx'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Signup from './signup.jsx'
+import Signup from './Signup.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Router>
